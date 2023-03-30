fffeb274cd85acfc898f95ce0391021cc7c2305d
v.ignatyev <ignatevvk@mail.ru>
2023-03-30 16:33:56 +0700

    UI 30.03 почти готов

--- src/main/java/ru/sber/spring/java13springmy/sdproject/service/GroupService.java
+++ src/main/java/ru/sber/spring/java13springmy/sdproject/service/GroupService.java
@@ -2,18 +2,36 @@ package ru.sber.spring.java13springmy.sdproject.service;
 
 import org.springframework.stereotype.Service;
 import ru.sber.spring.java13springmy.sdproject.dto.GroupDTO;
-import ru.sber.spring.java13springmy.sdproject.dto.GroupWithRolesDTO;
+import ru.sber.spring.java13springmy.sdproject.dto.RoleDTO;
 import ru.sber.spring.java13springmy.sdproject.mapper.GroupMapper;
+import ru.sber.spring.java13springmy.sdproject.mapper.RoleMapper;
 import ru.sber.spring.java13springmy.sdproject.model.Group;
 import ru.sber.spring.java13springmy.sdproject.repository.GroupRepository;
+import ru.sber.spring.java13springmy.sdproject.repository.RoleRepository;
+
+import java.util.List;
+
 @Service
 public class GroupService  extends GenericService<Group, GroupDTO> {
-   // private final GroupMapper groupMapper;
+   private final GroupMapper groupMapper;
+   private final RoleMapper roleMapper;
+   private  final RoleRepository roleRepository;
     protected GroupService (GroupRepository groupRepository,
-                            GroupMapper groupMapper) {
+                            GroupMapper groupMapper, RoleMapper roleMapper, RoleRepository roleRepository) {
         super(groupRepository,groupMapper);
 
-      //  this.groupMapper = groupMapper;
+       this.groupMapper = groupMapper;
+        this.roleMapper = roleMapper;
+
+        this.roleRepository = roleRepository;
+    }
+
+//    public List<TaskWithUserDTO> getAllTaskWithUser() {
+//        return taskWithUserMapper.toDTOs(taskRepository.findAll());
+//    }
+
+    public List<RoleDTO> getAllRoles() {
+        return roleMapper.toDTOs(roleRepository.findAll());
     }
 //    public GroupWithRolesDTO getGroupWithRole (Integer id) {
 //        return (GroupWithRolesDTO) groupMapper.toDto(mapper.toEntity(super.getOne(Long.valueOf(id))));
